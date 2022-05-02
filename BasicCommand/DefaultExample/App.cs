3e18767527a8a76f99e641049978ac06fb1cb190
Alexander Novikov <nvkv_ai@outlook.com>
2022-05-02 18:53:20 +0300

    add default example

---
+++ BasicCommand/DefaultExample/App.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Autodesk.Revit.UI;
+
+namespace BasicCommand.DefaultExample
+{
+    public class App : IExternalApplication
+    {
+        private const string TabName = "CustomTools";
+        private const string PanelName = "Graphics";
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            application.CreateRibbonTab(TabName);
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyPath = assembly.Location;
+
+            var graphicsPanel = application.CreateRibbonPanel(TabName, PanelName);
+
+            const string autoViewsCommandName = "Views";
+            
+            var autoViewsLargeImage = new BitmapImage(
+                new Uri($"pack://application:,,,/{assembly.GetName().Name};component/Resources/unknown.png"));
+
+            var autoViewsPushButtonData = new PushButtonData(autoViewsCommandName, autoViewsCommandName, assemblyPath,
+                "BasicCommand.DefaultExample.ViewsCreation")
+            {
+                LargeImage = autoViewsLargeImage,
+                Image = new TransformedBitmap(autoViewsLargeImage, new ScaleTransform(0.5, 0.5)),
+                ToolTip = "Views creation by elements of selected category",
+                LongDescription = "1.0"
+            };
+            
+            
+            graphicsPanel.AddItem(autoViewsPushButtonData);
+
+            return Result.Succeeded;
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ BasicCommand/DefaultExample/ViewsCreation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using BasicCommand.Abstract;
+
+namespace BasicCommand.DefaultExample
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ViewsCreation : IExternalCommand
+    {
+        // if you need modeless window
+        private static Window _window = null;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                // implement your code for <ViewsCreation> here 
+
+                // ...
+
+                // if you need modeless window
+                if (_window is null)
+                {
+                    // _window = new ViewsCreationWindow(commandData)
+                    _window = new Window();
+                    _window.Show();
+
+                    _window.Closed += (o, e) => { _window = null; };
+                }
+                else
+                {
+                    _window.Activate();
+                    _window.WindowState = WindowState.Normal;
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception e)
+            {
+                TaskDialog.Show("Error", e.Message);
+                return Result.Failed;
+            }
+        }
+    }
+}
